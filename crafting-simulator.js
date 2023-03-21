290bacd9dd19a0880ef30ad95d30c7b9799c7e09
Furaken <91683497+Furaken@users.noreply.github.com>
2023-03-22 06:30:21 +0700

    Update crafting-simulator.js

--- crafting-simulator.js
+++ crafting-simulator.js
@@ -1,15 +1,6 @@
-// Original py script by ben is big#5831
-// Modified to js script by Furaken / sk#3334
-
 function craftingSimulator(chance, amount) {
-    var numCrafted = 0,
-        attNum = 0,
-        failSPS = 0,
-        highestFailStreak = 0,
-        failStreak = 0,
+    var numCrafted = attNum = failSPS = highestFailStreak = failStreak = chanceToGetAtLeast1 = 0,
         allAtt = [],
-        table = {},
-        chanceToGetAtLeast1 = 0,
         chance = chance / 100
         list = [0, 0, 0, 0, chance],
         consAmount = amount
@@ -31,7 +22,7 @@ function craftingSimulator(chance, amount) {
     for(let i = 5; i < consAmount; i++) list[i] = chance + (1 - chance) * (list[i-1] + list[i-2] + list[i-3] + list[i-4]) / 4
     chanceToGetAtLeast1 = list[list.length - 1] * 100;
 
-    table = {
+    let table = {
         "Success at attempt number" : allAtt.join(", "),
         "Total attempts" : attNum,
         "Petals crafted" : numCrafted,
@@ -44,3 +35,5 @@ function craftingSimulator(chance, amount) {
     }
     console.table(table)
 }
+
+// Crafting 100 petals with 2.4% chance: craftingSimulator(2.4, 100)
