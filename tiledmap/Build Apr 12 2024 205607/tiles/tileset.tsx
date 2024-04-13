78abc0610193375459bde57624947d9ea851b790
Furaken <91683497+Furaken@users.noreply.github.com>
2024-04-13 20:49:37 +0700

    Add files via upload

---
+++ tiledmap/Build Apr 12 2024 205607/tiles/tileset.tsx
@@ -0,0 +1,480 @@
+<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="256" tileheight="256" tilecount="77" columns="0" tilerendersize="grid">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="0"/>
+ <tile id="0" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="desert_c_0.svg"/>
+ </tile>
+ <tile id="1" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="desert_c_1.svg"/>
+ </tile>
+ <tile id="2" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="desert_c_2.svg"/>
+ </tile>
+ <tile id="3" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="desert_c_3.svg"/>
+ </tile>
+ <tile id="4" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="desert_c_4.svg"/>
+ </tile>
+ <tile id="5" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="grass_c_0.svg"/>
+ </tile>
+ <tile id="6" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="grass_c_1.svg"/>
+ </tile>
+ <tile id="7" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="grass_c_2.svg"/>
+ </tile>
+ <tile id="8" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="grass_c_3.svg"/>
+ </tile>
+ <tile id="9" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="ocean_c_0.svg"/>
+ </tile>
+ <tile id="10" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="ocean_c_1.svg"/>
+ </tile>
+ <tile id="11" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="ocean_c_2.svg"/>
+ </tile>
+ <tile id="12" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="ocean_c_3.svg"/>
+ </tile>
+ <tile id="13" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="pvp_c_0.svg"/>
+ </tile>
+ <tile id="14" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="pvp_c_1.svg"/>
+ </tile>
+ <tile id="15" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="pvp_c_2.svg"/>
+ </tile>
+ <tile id="16" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="pvp_c_3.svg"/>
+ </tile>
+ <tile id="17" class="tile">
+  <image width="256" height="256" source="castle_l_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="130.5" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="18" class="tile">
+  <image width="256" height="256" source="castle_tl_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0" y="0" width="130.5" height="130.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="castle_c_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="20" class="tile">
+  <image width="256" height="256" source="castle_tri_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="130.5" height="256"/>
+   <object id="8" x="130.5" y="125.5" width="125.5" height="130.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="21" class="tile">
+  <image width="256" height="256" source="dirt_l_0.svg"/>
+  <objectgroup draworder="index" id="3">
+   <object id="24" x="0" y="0">
+    <polygon points="0,0 130,0 130,6 152,37.5 152,87 127.5,121.5 144,163 124,191 124,209 144,232 129.5,251.5 129.5,256 0,256"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="22" class="tile">
+  <image width="256" height="256" source="dirt_tl_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 130,0 130,6 152,37.5 152,87.5 115,140 73.5,128 48.5,128.5 17.5,142 4.5,130 0,130"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23" class="tile">
+  <image width="256" height="256" source="dirt_tri_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="20" x="0" y="0">
+    <polygon points="0,0 130.5,0 130.5,6 152.5,37.5 152,85.5 162,88.5 166.5,101 175.5,113 200,127 228.5,116.5 250.5,125.5 256,126 256,256 0,256"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="dirt_c_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="25" class="tile">
+  <image width="256" height="256" source="fire_anthole.svg"/>
+ </tile>
+ <tile id="27" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="dirt2_c_0.svg"/>
+ </tile>
+ <tile id="28" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="dirt2_c_1.svg"/>
+ </tile>
+ <tile id="32" class="tile">
+  <image width="256" height="256" source="water_l_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 128.5,0 121.5,45 122.5,62.5 134.5,98 137,134.5 130,163.5 116.5,192.5 116.5,208 126.5,238 128,256 0,256"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33" class="tile">
+  <image width="256" height="256" source="water_tl_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 128,0 127.667,45 133,79.6667 134.333,103.667 127,127.667 112,143.333 97.3333,147.667 66.3333,144.667 20.3333,130 -0.333333,128"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34" class="tile">
+  <image width="256" height="256" source="water_tri_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 128,0 121.667,22.6667 118,44.3333 121,67.3333 130.333,85.3333 156.333,112.667 201.667,131.667 228.333,132 256,127.667 256,256 0,256"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="35" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="water_c_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <image width="256" height="256" source="bridge_c_0.svg"/>
+ </tile>
+ <tile id="41">
+  <image width="256" height="256" source="bridge_r_0.svg"/>
+ </tile>
+ <tile id="42">
+  <image width="256" height="256" source="bridge_c_1.svg"/>
+ </tile>
+ <tile id="45">
+  <image width="256" height="256" source="desert_r_0.svg"/>
+ </tile>
+ <tile id="46">
+  <image width="256" height="256" source="desert_l_0.svg"/>
+ </tile>
+ <tile id="47">
+  <image width="256" height="256" source="desert_b_0.svg"/>
+ </tile>
+ <tile id="48">
+  <image width="256" height="256" source="sewer_entrance.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="60.8387" y="210.519">
+    <polygon points="0,0 12.5619,-12.9806 4.1873,-28.0549 9.21207,-44.3854 40.6168,-62.3907 92.5391,-55.6911 125.2,-32.2422 128.969,-27.6362 119.757,-11.3057 135.668,-2.93111 148.23,-28.0549 151.162,-79.9772 145.299,-103.426 137.344,-115.988 137.344,-146.137 126.456,-174.61 106.776,-195.966 78.3027,-206.852 53.5974,-206.852 25.5425,-195.547 4.1873,-171.679 -3.76857,-149.068 -4.1873,-115.988 -12.5619,-101.751 -18.0054,-81.2334 -18.0054,-43.1292 -12.5619,-18.4241"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <image width="256" height="256" source="cobblestone_c_0.svg"/>
+ </tile>
+ <tile id="50" class="tile">
+  <image width="256" height="256" source="sewage_l_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 128.5,0 121.5,45 122.5,62.5 134.5,98 137,134.5 130,163.5 116.5,192.5 116.5,208 126.5,238 128,256 0,256"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="51" class="tile">
+  <image width="256" height="256" source="sewage_tl_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 128,0 127.667,45 133,79.6667 134.333,103.667 127,127.667 112,143.333 97.3333,147.667 66.3333,144.667 20.3333,130 -0.333333,128"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="52" class="tile">
+  <image width="256" height="256" source="sewage_tri_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 128,0 121.667,22.6667 118,44.3333 121,67.3333 130.333,85.3333 156.333,112.667 201.667,131.667 228.333,132 256,127.667 256,256 0,256"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="53" class="tile">
+  <properties>
+   <property name="covers_everything" type="bool" value="true"/>
+  </properties>
+  <image width="256" height="256" source="sewage_c_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <image width="256" height="256" source="grate_c_0.svg"/>
+ </tile>
+ <tile id="55">
+  <image width="256" height="256" source="grate_l_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="14" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <image width="256" height="256" source="grate_tr_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="242" y="0" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <image width="256" height="256" source="grate_tli_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="14" height="256"/>
+   <object id="2" x="0" y="0" width="256" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <image width="256" height="256" source="sewer_entrance2.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="54.5" y="94">
+    <polygon points="-0.727273,-0.545455 13.5,10 24,29.5 52,44 99.5,45.5 123,30 136,13 148,0.5 157,19.5 156.5,54 143,76.5 143,100.5 129.5,130.5 105,151 77.5,157.5 52.5,154.5 24.5,137.5 8,114 3.5,89.5 3.5,76.5 -8.5,56.5 -11,34.5 -9,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <image width="256" height="256" source="anthole.svg"/>
+ </tile>
+ <tile id="60">
+  <image width="256" height="256" source="hel_c_0.svg"/>
+ </tile>
+ <tile id="61">
+  <image width="256" height="256" source="hel_c_1.svg"/>
+ </tile>
+ <tile id="62">
+  <image width="256" height="256" source="hel_c_2.svg"/>
+ </tile>
+ <tile id="63">
+  <image width="256" height="256" source="hel_c_3.svg"/>
+ </tile>
+ <tile id="64">
+  <image width="256" height="256" source="hel_c_4.svg"/>
+ </tile>
+ <tile id="65">
+  <image width="256" height="256" source="grass2_c_0.svg"/>
+ </tile>
+ <tile id="66">
+  <image width="256" height="256" source="grass2_c_1.svg"/>
+ </tile>
+ <tile id="67">
+  <image width="256" height="256" source="grass2_c_2.svg"/>
+ </tile>
+ <tile id="68">
+  <image width="256" height="256" source="grass2_c_3.svg"/>
+ </tile>
+ <tile id="69">
+  <image width="256" height="256" source="desert_t_0.svg"/>
+ </tile>
+ <tile id="70">
+  <image width="256" height="256" source="termite_mound.svg"/>
+ </tile>
+ <tile id="71">
+  <image width="256" height="256" source="hut_c_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="72">
+  <image width="256" height="256" source="hut_b_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="128" width="256" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="73">
+  <image width="256" height="256" source="hut_bl_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="128" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="74">
+  <image width="256" height="256" source="hut_bli_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="128" height="256"/>
+   <object id="2" x="128" y="128" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="75">
+  <image width="256" height="256" source="hut_br_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="128" y="128" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <image width="256" height="256" source="hut_bri_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="128" y="0" width="126" height="256"/>
+   <object id="2" x="0" y="128" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <image width="256" height="256" source="hut_l_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="128" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="78">
+  <image width="256" height="256" source="hut_r_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="128" y="0" width="128" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="79">
+  <image width="256" height="256" source="hut_t_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="256" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <image width="256" height="256" source="hut_tl_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <image width="256" height="256" source="hut_tli_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="128" height="256"/>
+   <object id="2" x="128" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <image width="256" height="256" source="hut_tr_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="128" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <image width="256" height="256" source="hut_tri_0.svg"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="256" height="128"/>
+   <object id="2" x="128" y="128" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <image width="256" height="256" source="wood_c_0.svg"/>
+ </tile>
+ <tile id="85">
+  <image width="256" height="256" source="grass2_b_0.svg"/>
+ </tile>
+ <tile id="86">
+  <image width="256" height="256" source="grass2_t_0.svg"/>
+ </tile>
+ <wangsets>
+  <wangset name="Everything" type="corner" tile="-1">
+   <wangcolor name="Castle" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Dirt" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Water" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="Grate" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="Hut" color="#00e9ff" tile="-1" probability="1"/>
+   <wangtile tileid="17" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="22" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="23" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="24" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="32" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="33" wangid="0,0,0,0,0,0,0,3"/>
+   <wangtile tileid="34" wangid="0,0,0,3,0,3,0,3"/>
+   <wangtile tileid="35" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="54" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="55" wangid="0,0,0,0,0,4,0,4"/>
+   <wangtile tileid="56" wangid="0,4,0,0,0,0,0,0"/>
+   <wangtile tileid="57" wangid="0,4,0,0,0,4,0,4"/>
+   <wangtile tileid="71" wangid="0,5,0,5,0,5,0,5"/>
+   <wangtile tileid="72" wangid="0,0,0,5,0,5,0,0"/>
+   <wangtile tileid="73" wangid="0,0,0,0,0,5,0,0"/>
+   <wangtile tileid="74" wangid="0,0,0,5,0,5,0,5"/>
+   <wangtile tileid="75" wangid="0,0,0,5,0,0,0,0"/>
+   <wangtile tileid="76" wangid="0,5,0,5,0,5,0,0"/>
+   <wangtile tileid="77" wangid="0,0,0,0,0,5,0,5"/>
+   <wangtile tileid="78" wangid="0,5,0,5,0,0,0,0"/>
+   <wangtile tileid="79" wangid="0,5,0,0,0,0,0,5"/>
+   <wangtile tileid="80" wangid="0,0,0,0,0,0,0,5"/>
+   <wangtile tileid="81" wangid="0,5,0,0,0,5,0,5"/>
+   <wangtile tileid="82" wangid="0,5,0,0,0,0,0,0"/>
+   <wangtile tileid="83" wangid="0,5,0,5,0,0,0,5"/>
+  </wangset>
+  <wangset name="Edge" type="edge" tile="-1">
+   <wangcolor name="Bridge" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="40" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="41" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="42" wangid="0,0,1,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
+,
+   
---
+++ tiledmap/Build Apr 12 2024 205607/tiles/tileset_diagonal.tsx
@@ -0,0 +1,3 @@
+<tileset version="1.9" tiledversion="1.9.2" name="tileset_diagonal" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+,
+   
