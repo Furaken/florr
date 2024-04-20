9d8acf3b9e2bf587d57d60f75b2e673e08b1bca4
Furaken <91683497+Furaken@users.noreply.github.com>
2024-04-20 18:19:35 +0700

    Add files via upload

---
+++ script/Bad Apple!! in mob gallery/script.js
@@ -0,0 +1,10 @@
+var rock = 17070809 // This id changes every update so the script will not work later, you need to find new id by yourself
+for (let i = rock; i < rock + 58; i++) Module.HEAPU8[i] = 255
+let j = 0
+setInterval(() => {
+    if (!(j < data.length)) return;
+    [40, 9, 5, 37, 45, 4, 55, 10].forEach((id, index) => {
+        Module.HEAPU8[rock - 1 + id] = (Number("0b" + data[j][index]) == 0 ? 1 : Number("0b" + data[j][index]))
+    })
+    j++
+}, 17) // Actually it is 16.9949788 but setInterval is gay
